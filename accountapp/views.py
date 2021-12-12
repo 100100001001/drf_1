84bdb789700faf9b8e7638057809c0986c05c8ef
hehe <ppangkko@gmail.com>
2021-12-13 00:16:12 +0900

    Templates, Design, Model, Account CreateView update

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
+from accountapp.models import NewModel
 
 
 def hello_world(request):
@@ -9,6 +15,19 @@ def hello_world(request):
     if request.method == 'POST':
         input_data = request.POST.get('input_data')
 
-        return render(request, 'accountapp/hello_world.html', context={'message': input_data})
+        new_model = NewModel()
+        new_model.text = input_data
+        new_model.save()
 
-    return render(request, 'accountapp/hello_world.html')
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+    new_model_list = NewModel.objects.all()
+    return render(request, 'accountapp/hello_world.html',
+                  context={'new_model_list': new_model_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
