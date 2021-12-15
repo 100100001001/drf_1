ffca044596d21c89f65651804416dc6cf7f5506b
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2021-12-15 16:46:43 +0900

    Profile Update View update

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,20 +2,30 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from profileapp.forms import ProfileCreationView
+from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
 class ProfileCreateView(CreateView):
     model = Profile
-    form_class = ProfileCreationView
+    form_class = ProfileCreationForm
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.owner.pk})
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
 
     def get_success_url(self):
         return reverse('accountapp:detail', kwargs={'pk': self.object.owner.pk})
